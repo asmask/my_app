69ad5883021aa31fa9422a2222527523d1cddfd8
com.sa <35959021+asmask@users.noreply.github.com>
2020-05-14 02:55:58 +0200

    Update config.php

--- config.php
+++ config.php
@@ -8,9 +8,11 @@ define('DB_NAME', 'my_db');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+//$link= new PDO('mysql:host=192.168.56.101;dbname=my_db','root','');
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
+
